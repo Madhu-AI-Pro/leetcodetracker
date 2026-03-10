4c92a8c15f3681ce2693a3fd3787640eae7595a1
Madhushree <24149027@drngpit.ac.in>
2026-03-10 15:14:30 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.java
@@ -0,0 +1,17 @@
+// Last updated: 3/10/2026, 3:14:47 PM
+class Solution {
+    public int maxSubArray(int[] nums) {
+         int maxSum = nums[0];
+        // Initialize the current sum of our subarray as nums[0]...
+        int currSum = nums[0];
+        // Traverse all the element through the loop...
+        for (int i = 1; i < nums.length; i++) {
+            // Do sum of elements contigous with curr sum...
+            // Compare it with array element to get maximum result...
+            currSum = Math.max(currSum + nums[i], nums[i]);
+            // Compare current sum and max sum.
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;      // return the contiguous subarray which has the largest sum...
+    }
+}
