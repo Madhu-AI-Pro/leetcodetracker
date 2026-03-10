fd047e9358136ec9b4f187c70b16035995f7d2e3
Madhushree <24149027@drngpit.ac.in>
2026-03-10 15:13:40 +0530

    Create 2630-AlternatingDigitSum

---
+++ 2630-AlternatingDigitSum/2630-AlternatingDigitSum.java
@@ -0,0 +1,16 @@
+// Last updated: 3/10/2026, 3:13:57 PM
+class Solution {
+    public int alternateDigitSum(int n) {
+        int i=0,sum=0;
+        while (n>0){
+            if (i%2==0)
+                sum+=n%10;
+            else
+                sum+=(n%10)*-1;
+            i++;
+            n/=10;
+        }
+        if (i%2==0) return sum*-1;
+        return sum;  
+    }
+}
