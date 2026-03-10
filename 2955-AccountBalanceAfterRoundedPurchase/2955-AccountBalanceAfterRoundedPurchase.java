b154fdeed41ce423c7d948efbf4b0be18bedc33c
Madhushree <24149027@drngpit.ac.in>
2026-03-10 15:13:30 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,7 @@
+// Last updated: 3/10/2026, 3:13:47 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        return  100-((purchaseAmount + 5) /10) *10;
+        
+    }
+}
