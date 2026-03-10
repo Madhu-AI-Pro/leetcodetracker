5fa2ae78079d7b3a57b3382a28efad204b5ec3a9
Madhushree <24149027@drngpit.ac.in>
2026-03-10 15:14:33 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,11 @@
+// Last updated: 3/10/2026, 3:14:51 PM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        String s=Integer.toString(x);
+        String reversed=new StringBuilder(s).reverse().toString();
+        return s.equals(reversed);
+    }
+}
