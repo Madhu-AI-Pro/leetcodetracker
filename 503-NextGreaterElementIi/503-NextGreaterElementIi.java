0146d9c4343406c8003f8369b18a1445d116a1dd
Madhushree <24149027@drngpit.ac.in>
2026-03-10 15:14:11 +0530

    Create 503-NextGreaterElementIi

---
+++ 503-NextGreaterElementIi/503-NextGreaterElementIi.java
@@ -0,0 +1,19 @@
+// Last updated: 3/10/2026, 3:14:28 PM
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+         int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int curr = nums[i % n];
+            while (!stack.isEmpty() && stack.peek() <= curr) {
+                stack.pop();
+            }
+            if (i < n) {
+                res[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+            stack.push(curr);
+        }
+        return res;
+    }
+}
